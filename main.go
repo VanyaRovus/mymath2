a53469b369845171c3210f26275bd2641dd49f3d
Vanya <vans00500@mail.ru>
2024-05-22 15:58:06 +0300

    1

---
+++ main.go
@@ -0,0 +1,37 @@
+package mymath2
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+func Yn(x int, y float64) float64 {
+	return math.Yn(x, y)
+}
